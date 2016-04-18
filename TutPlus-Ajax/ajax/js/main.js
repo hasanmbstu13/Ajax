4c0728e946c5bf9925e896839010576cf634a9ab
hasanmbstu13 <hasanmbstu13@gmail.com>
2016-04-18 12:29:01 +0600

    In this example we load html file by using ajax

---
+++ TutPlus-Ajax/ajax/js/main.js
@@ -0,0 +1,42 @@
+(function() {
+	// Target the element
+	var link = document.getElementsByTagName("a")[0];
+
+	link.onclick = function() {
+		// XHR Object
+		var xhr = new XMLHttpRequest();
+
+		// handle the "onreadystatechange" event
+		// xhr.readyState property values
+		// 0 = uninitialized basically meaning that the request hasn't open yet
+		// 1 = Loading basically meaning the request has been opened
+		// 2 = Loaded that means request has been sent to the server
+		// 3 = Interactive i.e server process the request & reponse back
+		// 4 = Complete i.e. request is finished we access to the server response
+
+		// open the request
+		// first one is method second one is file & third is request is asynchronous or synchrounous
+		// by default asynchronous & true means asynchronous
+		
+
+		xhr.onreadystatechange = function(){
+			if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
+				xhr.responseText;
+
+				var body = document.getElementsByTagName("body")[0];
+				var p = document.createElement("p");
+				var pText = document.createTextNode(xhr.responseText);
+				p.appendChild(pText);
+				body.appendChild(p);
+			}
+		};
+		
+		xhr.open("GET", "files/ajax.txt", true);
+		// send the request
+		// xhr.send(null) is null because our request is get not a post method
+		xhr.send(null);
+
+		// In able to disable the default behavior of our link	
+		return false;  
+	}
+})();
